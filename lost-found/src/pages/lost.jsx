4c7d059ad25e1ad1e3871a23ec46607f737d98b9
Millos Lucic <milos.lucic192@gmail.com>
2020-08-06 11:19:48 +0200

    Card

--- lost-found/src/pages/lost.jsx
+++ lost-found/src/pages/lost.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import Card from 'react-bootstrap/Card';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 class Lost extends React.Component {
 
   render() {
     return (
         <div>
-          <p>Lost</p>
+           <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+               <Card.Body>
+               <Card.Title>Lost </Card.Title>
+            <Card.Text>
+             Some quick example text to build on the card title and make up.
+            </Card.Text>
+            </Card.Body>
+           <ListGroup className="list-group-flush">
+          <ListGroupItem>Cras justo odio</ListGroupItem>
+        </ListGroup>
+     <Card.Body>
+    <Card.Link href="#">Lost Link</Card.Link>
+  </Card.Body>
+</Card>
         </div>
   
       );
