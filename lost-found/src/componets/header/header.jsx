8dfbbb325a7c48b2679aa40bbff1025ed78d2293
Millos Lucic <milos.lucic192@gmail.com>
2020-08-06 08:42:37 +0200

    menu

--- lost-found/src/componets/header/header.jsx
+++ lost-found/src/componets/header/header.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Menu from '../menu/menu.jsx';
 
+import {  HeaderS } from './header.styles';
+
 
 class Header extends React.Component  {
 
     render(){
           return (
+            <HeaderS>
              <Menu></Menu> 
+             </HeaderS>
           );
     }
     
--- lost-found/src/componets/menu/menu.jsx
+++ lost-found/src/componets/menu/menu.jsx
@@ -1,47 +1,58 @@
 import React from 'react';
 
+import { Lost } from './menu.styles';
+import { Text } from './menu.styles';
+import { Found } from './menu.styles';
+import {AboutAs } from './menu.styles';
+import {Card } from './menu.styles';
+
+import {ModalStyled } from '../modal/modal.styles';
+
 import ModalEx from '../modal/modal.jsx';
 import { BrowserRouter as Router ,Route ,Link ,Switch } from 'react-router-dom';
 
 import Lost1 from '../../pages/lost';
 import Found1 from '../../pages/found';
-import AboutAs from '../../pages/abouteAs';
+import AbouteAs from '../../pages/abouteAs';
 
 
 class Menu extends React.Component {
 
     render(){
         return (  <div>
-                      {/* <Text>Pronadji.me</Text> */}
-                    <ModalEx></ModalEx>
-                       
-                    <Router>
-    
-     {/* <Link  to="/abouteAs"  ><AboutAs>About as</AboutAs></Link> */}
-     {/* <Link  to="/abouteAs"  >About as</Link> */}
- 
-    {/* <Link  to='/lost'><Lost>lost</Lost></Link>  */}
+                      <Text>Pronadji.me</Text>
 
-    <Link  to='/lost'>lost</Link>
+                  <ModalStyled><ModalEx></ModalEx></ModalStyled> 
+                       
+      <Router>
+    
+     <Link  to="/abouteAs"  ><AboutAs>About as</AboutAs></Link>
+    
+    <Link  to='/lost'><Lost>lost</Lost></Link> 
+
+    <Link  to='/found'><Found>found</Found></Link>
    
-    {/* <Link  to='/found'><Found>found</Found></Link> */}
-       <Link  to='/found'>found</Link>
-   
-  
 
         <Switch>
+
           <Route path="/abouteAs">
-          {/* <Card><AbouteAs /></Card> */}
-          <AboutAs/>
+
+            <Card><AbouteAs /></Card>
+
           </Route>
+
           <Route path="/lost">
-           {/* <Card><Lost1 /></Card> */}
-           <Lost1 />
+
+             <Card><Lost1 /></Card>
+          
           </Route>
+
           <Route path="/found">
-          {/* <Card><Found1 /></Card>  */}
-          <Found1 />
+
+          <Card><Found1 /></Card> 
+
           </Route>
+
         </Switch>
 
 
