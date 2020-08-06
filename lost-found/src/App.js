69710464afa0bc9959889c0daa33efdddf276e61
Millos Lucic <milos.lucic192@gmail.com>
2020-08-06 09:33:01 +0200

    reducers

--- lost-found/src/App.js
+++ lost-found/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     
     <div className="App">
-      s <GlobalStyle/>
+       <GlobalStyle/>
       <Header></Header>
     </div>
   );
--- lost-found/src/index.js
+++ lost-found/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+
+import {Provider} from 'react-redux';
+
+import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux'
+import todoApp from './reducers'
+const store = createStore(todoApp)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
