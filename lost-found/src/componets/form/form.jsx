2ef870d8d2a08d905d3bd27e37484bbf72b41ab8
Millos Lucic <milos.lucic192@gmail.com>
2020-08-06 08:03:59 +0200

    form

--- lost-found/src/componets/form/form.jsx
+++ lost-found/src/componets/form/form.jsx
@@ -1,15 +1,88 @@
 import React from 'react';
 
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import Button from 'react-bootstrap/Button';
+
+import { ButtonE } from './form.styles';
+
 class FormItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            firstName: "",
+            lastName: "",
+            addres: "",
+            phoneNum: ""
+           
+    
+        };
+    }
+
+        handleChange(event) {
+            this.setState({password: event.target.value});
+            
+            
+          }
+                  
+           handleSubmit (event){
+            this.props.createItem(this.state.addres , this.state.phoneNum , this.state.lastName , this.state.firstName);
+            event.preventDefault();
+          }
+            
+
+
 
     render() {
         return (
-             <p>form</p>
+            <FormControl  onSubmit={this.handleSubmit}>
+
+        
+            <Input id="my-input" aria-describedby="my-helper-text"  placeholder="Frist name"  
+            
+            name="fristName"
+            defaultValue={this.state.fristName}
+            type="text"
+            onChange={this.handleChange}
+            
+            /><br/>
+    
+           
+            <Input id="my-input" aria-describedby="my-helper-text"  placeholder="Last name"    
+          
+             name="lastName"
+            defaultValue={this.state.lastName}
+            type="text"
+            onChange={this.handleChange} /><br/>
+             
+           
+            <Input id="my-input" aria-describedby="my-helper-text"  placeholder="Address"
+            
+            name="addres"
+            defaultValue={this.state.addres}
+            type="text"
+            onChange={this.handleChange}
+               /><br/>
+    
+           
+            <Input id="my-input" aria-describedby="my-helper-text"  placeholder="Phone number" 
+            name="PhoneNum"
+            defaultValue={this.state.phoneNum}
+            type="text"
+            onChange={this.handleChange}
+              />
+          
+            <ButtonE> <Button   variant="primary" type="submit" value="Submit"  >Create</Button></ButtonE>
+    
+          </FormControl>
             );
         }
         
 
         
 }
+
 
 export default FormItem;
